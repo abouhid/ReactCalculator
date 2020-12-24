27ff410b22e393b0864d36d029ddd2e78c98c250
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-24 12:00:51 -0300

    working with eslint

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import React from 'react';
+
+const ButtonPanel = () => (
+  <>
+
+  </>
+);
+
+export default ButtonPanel;
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import React from 'react';
+
+const Display = () => (
+
+  <>
+    test
+  </>
+);
+
+export default Display;
