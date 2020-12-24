e73f9e49585285e7a66bd36ae92e67cf82c93d3f
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-24 13:12:25 -0300

    edit className of display

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import '../App.css';
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 const App = () => (
   <div className="App">
-    <Display name="test" />
+    <Display />
     <ButtonPanel />
 
   </div>
