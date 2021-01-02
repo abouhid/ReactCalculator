8873bf659b3c8f03b795101e34e4ec8c048519f6
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-02 09:43:23 -0300

    implemented and passed click handle

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,39 +1,44 @@
 import React from 'react';
-import Buttons from './Button';
+import PropTypes from 'prop-types';
+import Button from './Button';
 
-export default function ButtonPanel() {
+export default function ButtonPanel(props) {
+  const { onClick } = props;
   return (
     <div className="panel">
       <div className="group1">
-        <Buttons value="AC" />
-        <Buttons value="+/-" />
-        <Buttons value="%" />
-        <Buttons value="÷" />
+        <Button onClick={onClick} value="AC" />
+        <Button onClick={onClick} value="+/-" />
+        <Button onClick={onClick} value="%" />
+        <Button onClick={onClick} value="÷" />
       </div>
       <div className="group2">
-        <Buttons value="8" />
-        <Buttons value="9" />
-        <Buttons value="7" />
-        <Buttons value="X" />
+        <Button onClick={onClick} value="8" />
+        <Button onClick={onClick} value="9" />
+        <Button onClick={onClick} value="7" />
+        <Button onClick={onClick} value="X" />
       </div>
       <div className="group3">
-        <Buttons value="4" />
-        <Buttons value="5" />
-        <Buttons value="6" />
-        <Buttons value="-" />
+        <Button onClick={onClick} value="4" />
+        <Button onClick={onClick} value="5" />
+        <Button onClick={onClick} value="6" />
+        <Button onClick={onClick} value="-" />
       </div>
       <div className="group4">
-        <Buttons value="1" />
-        <Buttons value="2" />
-        <Buttons value="3" />
-        <Buttons value="+" />
+        <Button onClick={onClick} value="1" />
+        <Button onClick={onClick} value="2" />
+        <Button onClick={onClick} value="3" />
+        <Button onClick={onClick} value="+" />
       </div>
       <div className="group5">
-        <Buttons value="0" />
-        <Buttons value="." />
-        <Buttons value="=" />
+        <Button onClick={onClick} value="0" />
+        <Button onClick={onClick} value="." />
+        <Button onClick={onClick} value="=" />
 
       </div>
     </div>
   );
 }
+ButtonPanel.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
