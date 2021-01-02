39bae9f9113d078c5b0939662f7005e4428394b6
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-02 18:25:06 -0300

    Apply hooks in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = () => {
+  const [obj, setObj] = useState(
+    {
       total: null,
       next: null,
       operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+    },
+  );
+  const handleClick = btnName => {
+    const newState = Calculate(obj, btnName);
+    setObj(newState);
+  };
 
-  handleClick(btnName) {
-    const newState = Calculate(this.state, btnName);
-    this.setState(newState);
-  }
-
-  render() {
-    const { total, next } = this.state;
-
-    return (
-      <div className="App">
-        <h1>React Calculator</h1>
-        <Display amount={next || total || '0'} />
-        <ButtonPanel onClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>React Calculator</h1>
+      <Display amount={obj.next || obj.total || '0'} />
+      <ButtonPanel onClick={handleClick} />
+    </div>
+  );
+};
 
 export default App;
