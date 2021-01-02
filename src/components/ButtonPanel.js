97ef3330a0fd3ab86e0f80ee7d8e3d0f03107500
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-02 18:19:16 -0300

    button panel edit

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,39 +4,21 @@ import Button from './Button';
 
 const ButtonPanel = props => {
   const { onClick } = props;
-  return (
-    <div className="panel">
-      <div className="group1">
-        <Button onClick={onClick} value="AC" />
-        <Button onClick={onClick} value="+/-" />
-        <Button onClick={onClick} value="%" />
-        <Button onClick={onClick} value="÷" />
-      </div>
-      <div className="group2">
-        <Button onClick={onClick} value="8" />
-        <Button onClick={onClick} value="9" />
-        <Button onClick={onClick} value="7" />
-        <Button onClick={onClick} value="X" />
-      </div>
-      <div className="group3">
-        <Button onClick={onClick} value="4" />
-        <Button onClick={onClick} value="5" />
-        <Button onClick={onClick} value="6" />
-        <Button onClick={onClick} value="-" />
-      </div>
-      <div className="group4">
-        <Button onClick={onClick} value="1" />
-        <Button onClick={onClick} value="2" />
-        <Button onClick={onClick} value="3" />
-        <Button onClick={onClick} value="+" />
-      </div>
-      <div className="group5">
-        <Button onClick={onClick} value="0" />
-        <Button onClick={onClick} value="." />
-        <Button onClick={onClick} value="=" />
+  const butOptions = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-      </div>
+  const list = butOptions.map(value => (
+
+    <div key={value} className="panel-button">
+      <Button onClick={onClick} value={value} />
+
     </div>
+  ));
+
+  return (
+    <ul className="panel">
+      {list}
+
+    </ul>
   );
 };
 ButtonPanel.propTypes = {
